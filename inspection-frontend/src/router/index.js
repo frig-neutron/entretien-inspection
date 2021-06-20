dcd2b028a302cf4116baf0bc272fddd374ec76f5
Daniil Barklon <frig.neutron@gmail.com>
2021-06-20 12:09:38 -0400

    using history with unit form

--- inspection-frontend/src/router/index.js
+++ inspection-frontend/src/router/index.js
@@ -1,5 +1,7 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import {createRouter, createWebHashHistory, createWebHistory} from 'vue-router'
 import Home from '../views/Home.vue'
+import About from '../views/About.vue'
+import Unit from '../views/Unit.vue'
 
 const routes = [
   {
@@ -10,15 +12,18 @@ const routes = [
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: About
+  },
+  {
+    path :'/building/:building/unit/:unit',
+    name: 'Unit',
+    component: Unit,
+    props: true
   }
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes
 })
 
---
+++ inspection-frontend/src/views/Unit.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="backdrop">
+    <div class="modal" @click="hideUnits">
+      <p>{{ building }} {{ unit }}</p>
+      <slot name="occupant"><h2>no one here but us chickens</h2></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Unit",
+  props: ["building", "unit"],
+  methods: {
+    hideUnits(){
+      this.$emit('close')
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
