5a05018ad0998bf660b7b24c8bee99ea81fc3808
Daniil Barklon <frig.neutron@gmail.com>
2021-06-20 15:46:11 -0400

    list buildings/unit

--- inspection-frontend/src/views/Home.vue
+++ inspection-frontend/src/views/Home.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="home">
+    <div v-for="building in buildings" class="building">
+      <div>{{building.number}}</div>
+      <ol>
+        <li v-for="unit in building.units">
+          <router-link :to="{
+            name: 'Unit',
+            params: { building: building.number, unit: unit.number }
+          }">Unit {{unit.number}}</router-link>
+        </li>
+      </ol>
+    </div>
   </div>
 </template>
 
@@ -8,6 +19,119 @@
 export default {
   name: 'Home',
   components: {
-  }
+  },
+  data() {
+    return {
+      buildings: [
+        {
+          number: 3735,
+          units: [
+            {
+              number: 1
+            },
+            {
+              number: 2
+            },
+            {
+              number: 3
+            },
+            {
+              number: 4
+            },
+            {
+              number: 5
+            },
+            {
+              number: 6
+            }
+          ]
+        },
+        {
+          number: 3737,
+          units: [
+            {
+              number: 1
+            },
+            {
+              number: 2
+            },
+            {
+              number: 3
+            },
+            {
+              number: 4
+            },
+            {
+              number: 5
+            },
+            {
+              number: 6
+            }
+          ]
+        },
+        {
+          number: 3739,
+          units: [
+            {
+              number: 1
+            },
+            {
+              number: 2
+            },
+            {
+              number: 3
+            },
+            {
+              number: 4
+            },
+            {
+              number: 5
+            },
+          ]
+        },
+        {
+          number: 3743,
+          units: [
+            {
+              number: 1
+            },
+            {
+              number: 2
+            },
+            {
+              number: 3
+            },
+          ]
+        },
+        {
+          number: 3745,
+          units: [
+            {
+              number: 1
+            },
+            {
+              number: 2
+            },
+            {
+              number: 3
+            },
+          ]
+        },
+      ]
+    }
+  },
 }
 </script>
+
+<style scoped>
+  ol > li {
+    list-style: none;
+  }
+  .home {
+    position: relative;
+  }
+  .building {
+    display: inline-block;
+    vertical-align: top;
+  }
+</style>
