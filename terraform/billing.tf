894133f4631b031b16ee4fe178d5e8b2995a2c6d
Daniil Barklon <frig.neutron@gmail.com>
2022-02-26 14:49:15 -0500

    remove budget

--- terraform/billing.tf
+++ terraform/billing.tf
@@ -2,15 +2,3 @@ data "google_billing_account" "default" {
   billing_account = "billingAccounts/011E93-94AD19-D2FF71"
 }
 
-resource "google_billing_budget" "seatbelt" {
-  billing_account = data.google_billing_account.default.id
-  amount {
-    specified_amount {
-      currency_code = "CAD"
-      units         = "20"
-    }
-  }
-  threshold_rules {
-    threshold_percent = 0.5
-  }
-}
