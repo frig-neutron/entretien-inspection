d6dc34845d05307516227556107e3c76eebdca40
Daniil Barklon <frig.neutron@gmail.com>
2022-02-26 14:12:36 -0500

    attach billing account to project

---
+++ terraform/billing.tf
@@ -0,0 +1,3 @@
+data "google_billing_account" "default" {
+  billing_account = "billingAccounts/011E93-94AD19-D2FF71"
+}
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,6 @@
 resource "google_project" "inspection" {
   name       = "inspection-dev"
   project_id = "inspection-dev-7b978"
+
+  billing_account = data.google_billing_account.default.id
 }
